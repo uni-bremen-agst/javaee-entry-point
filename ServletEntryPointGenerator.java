bb5c3c9cfabf3e9414dd44410f5076e4e2f9a1d9
Marc-André Laverdière <marc-andre@atc.tcs.com>
2013-02-04 11:20:46 -0500

    (WIP) Support for @WebService and @WebMethod

--- ServletEntryPointGenerator.java
+++ ServletEntryPointGenerator.java
@@ -1,6 +1,12 @@
 package soot.jimple.toolkits.javaee;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,14 +17,17 @@ import java.util.Map;
 
 import soot.BooleanType;
 import soot.DoubleType;
+import soot.FastHierarchy;
 import soot.FloatType;
 import soot.G;
+import soot.Hierarchy;
 import soot.IntType;
 import soot.Local;
 import soot.LongType;
 import soot.Modifier;
 import soot.PhaseOptions;
 import soot.PrimType;
+import soot.Printer;
 import soot.RefType;
 import soot.Scene;
 import soot.SceneTransformer;
@@ -43,9 +52,12 @@ import soot.jimple.toolkits.javaee.model.servlet.Listener;
 import soot.jimple.toolkits.javaee.model.servlet.Servlet;
 import soot.jimple.toolkits.javaee.model.servlet.Web;
 import soot.jimple.toolkits.javaee.model.servlet.io.WebXMLReader;
+import soot.jimple.toolkits.typing.ClassHierarchy;
+import soot.options.Options;
 import soot.tagkit.AnnotationTag;
 import soot.tagkit.VisibilityAnnotationTag;
 import soot.util.Chain;
+import soot.util.EscapedWriter;
 
 /**
  * This class drives the generation of a main method that creates and calls
@@ -471,25 +483,41 @@ public class ServletEntryPointGenerator extends SceneTransformer implements Sign
 	  Collection<SootClass> wsClasses = new ArrayList<SootClass>();
 	  Collection<SootMethod> wsMethods = new ArrayList<SootMethod>();
 	  
+	  Hierarchy cha = Scene.v().getActiveHierarchy();
+	  SootClass servletClass = Scene.v().getSootClass(HTTP_SERVLET_CLASS_NAME);
+	  
 		for(final SootClass clazz : Scene.v().getApplicationClasses()) {
 		  if (!clazz.isConcrete()) //ignore interfaces and abstract classes
 		    continue;
-			if(isServlet(clazz)) { //for servlets
+			if (cha.isClassSubclassOf(clazz, servletClass)){//if(isServlet(clazz)) { //for servlets
 				registerServlet(clazz);
 			} else if (clazz.hasTag("VisibilityAnnotationTag")) { //for web services
         VisibilityAnnotationTag vat = (VisibilityAnnotationTag) clazz.getTag("VisibilityAnnotationTag");
         for (AnnotationTag at : vat.getAnnotations()) {
-          
+                  
           //Handling @WebService and @WebMethod
           if ("Ljavax/jws/WebService;".equals(at.getType())&& !clazz.isInterface()){
             wsClasses.add(clazz);
+            Collection<SootClass> interfaces = getClassesAnnotatedWithWebService(
+                                                  getInterfacesTransitively(clazz));
             for (SootMethod sm : clazz.getMethods()){
-              if (sm.hasTag("VisibilityAnnotationTag")){
-                VisibilityAnnotationTag vat2 = (VisibilityAnnotationTag) sm.getTag("VisibilityAnnotationTag");
-                for (AnnotationTag at2 : vat2.getAnnotations())
-                  if ("Ljavax/jws/WebMethod;".equals(at2.getType()))
-                    wsMethods.add(sm);
-              }        
+              
+              //Case #1: The annotation is on the class itself
+              if (getWebMethodAnnotation(sm) != null){
+                wsMethods.add(sm);
+                G.v().out.println("Found web method: " + sm);
+                continue;
+              }
+                          
+              //Case #2: The annotation is on the interface's method
+              for (SootClass sc : interfaces){
+                if (sc.declaresMethod(sm.getSubSignature()) 
+                    && getWebMethodAnnotation(sc.getMethod(sm.getSubSignature())) != null){
+                  wsMethods.add(sm);
+                  G.v().out.println("Found web method: " + sm);
+                  break; //no need to check the other interfaces
+                }
+              }
             }
 
           // Handling @WebServiceProvider
@@ -513,6 +541,56 @@ public class ServletEntryPointGenerator extends SceneTransformer implements Sign
 		
 	}
 
+	
+	/**
+	 * Filters a collection of classes to keep only those annotated with @WebService
+	 * @param classes the classes to filter
+	 * @return a non-null collection of classes meeting that criteria, possibly empty
+	 */
+	 private Collection<SootClass> getClassesAnnotatedWithWebService(Collection<SootClass> classes){
+	   Collection<SootClass> wsInterfaces = new ArrayList<SootClass>();
+     for (SootClass sc : classes){
+       if (isClassAnnotatedWithWebService(sc))
+         wsInterfaces.add(sc);
+        
+     }
+     return wsInterfaces;
+	 }
+	
+	 private boolean isClassAnnotatedWithWebService(SootClass sc){
+	   if (sc.hasTag("VisibilityAnnotationTag")){
+       VisibilityAnnotationTag vatInterface = (VisibilityAnnotationTag) sc.getTag("VisibilityAnnotationTag");
+       for (AnnotationTag atInterface : vatInterface.getAnnotations())
+         if ("Ljavax/jws/WebService;".equals(atInterface.getType()))
+           return true;
+	   }
+	   return false;
+	 }
+	 
+	 /**
+	  * Gets the @WebMethod annotation, if any
+	  * @param sm the method to check
+	  * @return <code>null</code> if the annotation is not found, the annotation otherwise.
+	  */
+	 private AnnotationTag getWebMethodAnnotation(SootMethod sm){
+     if (sm.hasTag("VisibilityAnnotationTag")){
+       VisibilityAnnotationTag vat = (VisibilityAnnotationTag) sm.getTag("VisibilityAnnotationTag");
+       for (AnnotationTag at : vat.getAnnotations())
+         if ("Ljavax/jws/WebMethod;".equals(at.getType()))
+           return at;
+     }
+     return null;
+	 }
+	 
+	private Collection<SootClass> getInterfacesTransitively(SootClass sc){
+	  final Collection<SootClass> retVal = new ArrayList<SootClass>();
+	  for (SootClass interf : sc.getInterfaces()){
+	    retVal.add(interf);
+	    retVal.addAll(getInterfacesTransitively(interf));
+	  }
+	  return retVal;
+	}
+	
 	/**
 	 * Registers a servlet as if it was declared in web.xml
 	 * @param clazz the class
@@ -560,7 +638,7 @@ public class ServletEntryPointGenerator extends SceneTransformer implements Sign
       final Local newLocal = serviceMethod.local(false, sc.getType());
       generatedLocals.put(sc, newLocal);
       serviceMethod.createInstance(newLocal, sc);
-      units.add(jimple.newInvokeStmt(jimple.newVirtualInvokeExpr(newLocal, sc.getMethod("void <init>()").makeRef())));
+      units.add(jimple.newInvokeStmt(jimple.newSpecialInvokeExpr(newLocal, sc.getMethod("void <init>()").makeRef())));
     }
     
     //Add the calls to the WS methods
@@ -587,14 +665,16 @@ public class ServletEntryPointGenerator extends SceneTransformer implements Sign
           
         } else if (t instanceof RefType){
           units.add(jimple.newAssignStmt(paramLocal, jimple.newNewExpr((RefType)t)));
-          units.add(jimple.newInvokeStmt(jimple.newVirtualInvokeExpr(paramLocal, ((RefType) t).getSootClass().getMethod("void <init>()").makeRef())));
+          units.add(jimple.newInvokeStmt(jimple.newSpecialInvokeExpr(paramLocal, ((RefType) t).getSootClass().getMethod("void <init>()").makeRef())));
         }
         arguments.add(paramLocal);//TODO
       }
       units.add(jimple.newInvokeStmt(jimple.newVirtualInvokeExpr(
           generatedLocals.get(sm.getDeclaringClass()), sm.makeRef(), arguments)));      
     }
-
+    PrintWriter pw = new PrintWriter(new EscapedWriter(new OutputStreamWriter(G.v().out)));
+    Printer.v().printTo(clazz, pw);
+    pw.close();
 
     return clazz;
 	}
