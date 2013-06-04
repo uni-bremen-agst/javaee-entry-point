33b755bdd0a4b51228b8def808c100224bb30136
Marc-André Laverdière <marc-andre@atc.tcs.com>
2013-06-04 14:20:48 -0400

    WIP Mavenization

---
+++ detectors/JBossWSTestDetector.scala
@@ -0,0 +1,9 @@
+package soot.jimple.toolkits.javaee.detectors
+
+/**
+ * (c) Copyright 2013, Tata Consultancy Services & Ecole Polytechnique de Montreal
+ * All rights reserved
+ */
+class JBossWSTestDetector {
+
+}
