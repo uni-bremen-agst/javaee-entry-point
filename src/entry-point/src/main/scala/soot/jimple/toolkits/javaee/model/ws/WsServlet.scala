6f29ac2905a9b00be8cb5bacfc90018917c599f7
Marc-André Laverdière <marc-andre@atc.tcs.com>
2014-09-16 09:04:52 -0400

    [Prototype] Fix for bug that WSCaller didn't have any calls to WSs. Added utility method to get all ws operations and output those in the template.

--- src/entry-point/src/main/scala/soot/jimple/toolkits/javaee/model/ws/WsServlet.scala
+++ src/entry-point/src/main/scala/soot/jimple/toolkits/javaee/model/ws/WsServlet.scala
@@ -5,7 +5,12 @@
 package soot.jimple.toolkits.javaee.model.ws
 
 import javax.xml.bind.annotation._
+
 import soot.jimple.toolkits.javaee.model.servlet.http.GenericServlet
+
+import scala.annotation.meta.beanGetter
+import scala.beans.BeanProperty
+import scala.collection.JavaConversions._
 
 /**
  * Representation of a servlet for web services.
@@ -13,14 +18,13 @@ import soot.jimple.toolkits.javaee.model.servlet.http.GenericServlet
  * @author Marc-André Laverdière-Papineau
  */
 @XmlRootElement(name = "WsServlet")
-case class WsServlet(services : java.util.List[WebService]) extends GenericServlet {
+case class WsServlet(
+                      @(XmlElementWrapper@beanGetter) @(XmlElement@beanGetter)(name = "service") @BeanProperty services: java.util.List[WebService])
+  extends GenericServlet {
 
   // JAXB-specific
   def this() = this(new java.util.ArrayList[WebService](0))
 
-  //We shouldn't need this, but the annotation magic is harder to handle
-  //than going for the simple def
-  @XmlElementWrapper
-  @XmlElement(name="service")
-  def getServices() : java.util.List[WebService] = services
+  @BeanProperty
+  lazy val operations: java.util.List[WebMethod] = services.flatMap(_.methods)
 }
